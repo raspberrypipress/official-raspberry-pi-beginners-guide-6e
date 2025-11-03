7900740a693b7922ee370794a41a89f328068a5e
Brian Jepson <bjepson@gmail.com>
2025-11-03 08:13:46 -0500

    to snake_case

--- en/code/ch05/text-adventure-final.py
+++ en/code/ch05/text-adventure-final.py
@@ -2,7 +2,7 @@
 
 # A simple text adventure you can enhance with your own code
 
-def showInstructions():
+def show_instructions():
     # print a main menu and the commands
     print("""
 Text Adventure
@@ -14,15 +14,15 @@ Commands:
   get [item]
 """)
 
-def showStatus():
+def show_status():
     # print the player's current status
     print("---------------------------")
-    print("You are in the " + currentRoom)
+    print("You are in the " + current_room)
     # show the current inventory
     print("Inventory : " + str(inventory))
     # show an item if there is one
-    if "item" in rooms[currentRoom]:
-        print("You see a " + rooms[currentRoom]['item'])
+    if "item" in rooms[current_room]:
+        print("You see a " + rooms[current_room]['item'])
     print("---------------------------")
 
 # an inventory, which is initially empty
@@ -53,14 +53,14 @@ rooms = {
 }
 
 # start the player in the Hall
-currentRoom = 'Hall'
+current_room = 'Hall'
 
-showInstructions()
+show_instructions()
 
 # loop forever
 while True:
 
-    showStatus()
+    show_status()
 
     # get the player's next 'move'
     # .split() breaks it up into an list array
@@ -75,36 +75,37 @@ while True:
     # if they type 'go' first
     if move[0] == 'go':
         # check that they are allowed wherever they want to go
-        if move[1] in rooms[currentRoom]:
+        if move[1] in rooms[current_room]:
             # set the current room to the new room
-            currentRoom = rooms[currentRoom][move[1]]
+            current_room = rooms[current_room][move[1]]
         # there is no door (link) to the new room
         else:
             print("You can't go that way!")
 
     # if they type 'get' first
     if move[0] == 'get':
-        # if the room contains an item, and the item is the one they want to get
-        if 'item' in rooms[currentRoom] and move[1] in rooms[currentRoom]['item']:
+        # if the room contains an item, and the item is the one they want
+        if ('item' in rooms[current_room] 
+              and move[1] in rooms[current_room]['item']):
             # add the item to their inventory
             inventory += [move[1]]
             # display a helpful message
             print(move[1] + " got!")
             # delete the item from the room
-            del rooms[currentRoom]['item']
+            del rooms[current_room]['item']
         # otherwise, if the item isn't there to get
         else:
             # tell them they can't get it
             print("Can't get " + move[1] + "!")
 
     # player loses if they enter a room with a monster
-    if ('item' in rooms[currentRoom]
-          and 'monster' in rooms[currentRoom]['item']):
+    if ('item' in rooms[current_room]
+          and 'monster' in rooms[current_room]['item']):
         print('A monster has got you... GAME OVER!')
         break
 
     # player wins if they get to the garden with a key and a potion
-    if (currentRoom == 'Garden' and 'key' in inventory
+    if (current_room == 'Garden' and 'key' in inventory
           and 'potion' in inventory):
        print('You escaped the house... YOU WIN!')
        break
